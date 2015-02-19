9036d24e58128f0dd1bb82d5a200fafce81e4a0f
Bborie Park <bboriepark@granular.ag>
2015-02-19 10:00:52 +0000

    0.1

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='pgpointcloud_utils',
-    version='0.0',
+    version='0.1',
     author='Bborie Park',
     author_email='bboriepark@granular.ag',
     url='www.granular.ag',
-    packages=['geojson2pgpc', 'ogr2pgpc'],
+    packages=['geojson2pgpc', 'ogr2pgpc', 'pgpointcloud_utils'],
     scripts=['scripts/geojson2pgpc', 'scripts/ogr2pgpc']
 )
